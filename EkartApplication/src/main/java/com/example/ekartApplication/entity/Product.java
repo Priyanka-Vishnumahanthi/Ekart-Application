4e25dee1405e6b534043bb9761c2b8d869691e63
pilli <pilli@DESKTOP-KPPK5JI>
2025-05-09 17:48:52 +0530

    testing

--- EkartApplication/src/main/java/com/example/ekartApplication/entity/Product.java
+++ EkartApplication/src/main/java/com/example/ekartApplication/entity/Product.java
@@ -4,7 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.*;
-
+//test
 @Entity
 public class Product {
 	@Id
